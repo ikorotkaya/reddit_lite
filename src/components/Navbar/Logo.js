be371ecf1c2a0254b5b2ed1d4aaf92161dfa6a65
ikorotkaya <irinkakorotkaya@gmail.com>
2023-02-26 14:24:23 +0100

    Update icon in navbar

--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -8,7 +8,7 @@ class Logo extends React.Component {
   render() {
     return (
       <div className="logo">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvp_7wmMJzFr_atRtvsRTHiTweuMarz4S3aw&usqp=CAU" alt="reddit-icon" className="logo__icon"></img>
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/008/385/783/small/reddit-social-media-design-icon-symbol-logo-illustration-free-vector.jpg" alt="reddit-icon" className="logo__icon"></img>
         <h3 className='logo__name'>Reddit<span>Lite</span></h3>
 
       </div>
