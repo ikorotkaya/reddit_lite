0cadb3faa9d967f9c9ddd31a661e81a471821748
ikorotkaya <irinkakorotkaya@gmail.com>
2023-02-28 14:01:38 +0100

    Add timestamp for Post components

--- src/components/Feed/PostDetails.js
+++ src/components/Feed/PostDetails.js
@@ -7,11 +7,16 @@ class PostDetails extends React.Component {
 
 
   render() {
+    const createdData = this.props.postData.created;
+    const d = new Date();
+    d.setTime(createdData);
+    const timestamp = d.toString().slice(16, 24);
+
     return (
       <div className="post-details">
         <div className='post-details__username'>{this.props.postData.author}</div>
-        <div className='post-details__time'>Time</div>
-        <PostDetailsComments comments={this.props.postData.num_comments}/>
+        <div className='post-details__time'>{timestamp}</div>
+        <PostDetailsComments comments={this.props.postData.num_comments} />
       </div>
     )
   }
