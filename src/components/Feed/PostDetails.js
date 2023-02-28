8d218e310e0a1ea16fea4b052fee498e97186143
ikorotkaya <irinkakorotkaya@gmail.com>
2023-02-28 15:54:41 +0100

    Change timestamp for relative time of the post according to unix timestamp

--- src/components/Feed/PostDetails.js
+++ src/components/Feed/PostDetails.js
@@ -8,9 +8,49 @@ class PostDetails extends React.Component {
 
   render() {
     const createdData = this.props.postData.created;
-    const d = new Date();
-    d.setTime(createdData);
-    const timestamp = d.toString().slice(16, 24);
+
+    // to show the exact time of the post
+    // const d = new Date();
+    // d.setTime(createdData);
+    // const time = d.toString().slice(16, 24);
+
+    function timeDifference(current, previous) {
+
+      var secPerMinute = 60;
+      var secPerHour = secPerMinute * 60;
+      var secPerDay = secPerHour * 24;
+      var secPerMonth = secPerDay * 30;
+      var secPerYear = secPerDay * 365;
+
+      var postTime = Math.abs(current - previous);
+
+      if (postTime < secPerMinute) {
+        return Math.round(postTime / 1000) + ' seconds ago';
+      }
+
+      else if (postTime < secPerHour) {
+        return Math.round(postTime / secPerMinute) + ' minutes ago';
+      }
+
+      else if (postTime < secPerDay) {
+        return Math.round(postTime / secPerHour) + ' hours ago';
+      }
+
+      else if (postTime < secPerMonth) {
+        return 'approximately ' + Math.round(postTime / secPerDay) + ' days ago';
+      }
+
+      else if (postTime < secPerYear) {
+        return 'approximately ' + Math.round(postTime / secPerMonth) + ' months ago';
+      }
+
+      else {
+        return 'approximately ' + Math.round(postTime / secPerYear) + ' years ago';
+      }
+    }
+
+    const currentSeconds = Date.now() / 1000;
+    const timestamp = timeDifference(currentSeconds, createdData);
 
     return (
       <div className="post-details">
