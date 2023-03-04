09c08d6a21f059b778d6df51906528b7afe1bfde
ikorotkaya <irinkakorotkaya@gmail.com>
2023-03-04 16:21:58 +0100

    Add choodeMediaType function to print different types of content in the posts

--- src/components/Feed/PostContent.js
+++ src/components/Feed/PostContent.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import './PostContent.scss';
 
-class PostContent extends React.Component {
-
+export default function PostContent(props) {
   // TODO: split into 4 components for VideoPost, CommentPost, ImagePost and ImageGalleryPost
-  render() {
-    return (
-      <div className="post-content">
-        <h2 className='post-content__title'>{this.props.postData.title}</h2>
-        {this.props.postData.secure_media?.reddit_video ? <video width="320" height="240" controls><source src={this.props.postData.secure_media.reddit_video.scrubber_media_url} type="video/mp4"/></video> : <img src={this.props.postData.thumbnail} alt="" className="post-content__image"/> }
-      </div>
-    )
-  }
-}
+  const { postData } = props;
 
-export default PostContent
+  function chooseMediaType(postData) {
+    if (postData.secure_media?.reddit_video) {
+      return <video width="320" height="240" controls><source src={postData.secure_media.reddit_video.scrubber_media_url} type="video/mp4" /></video>
+    } else {
+      return <img src={postData.thumbnail} alt="" className="post-content__image" />
+    }
+  }
+
+  return (
+    <div className="post-content">
+      <h2 className='post-content__title'>{postData.title}</h2>
+      {chooseMediaType(postData)}
+    </div>
+  )
+}
