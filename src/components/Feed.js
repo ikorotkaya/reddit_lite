8ac25b9f9b4c57b0a54c6218583a7f2ca8e246ed
ikorotkaya <irinkakorotkaya@gmail.com>
2023-03-05 18:45:13 +0100

    Write conditional statement in one line

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -7,11 +7,7 @@ class Feed extends React.Component {
   render() {
     const posts = Object.keys(this.props.posts).filter(postId => {
       const postTitle = this.props.posts[postId].title
-      if (postTitle.includes(this.props.searchTerm)) {
-        return true
-      } else {
-        return false
-      }
+      return postTitle.includes(this.props.searchTerm)
     }).map((postId) => {
       const postData = this.props.posts[postId]
       return <Post postData={postData} key={postId} />
