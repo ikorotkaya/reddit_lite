0cdc905831bf4fea31956555dd6b567a029a294a
ikorotkaya <irinkakorotkaya@gmail.com>
2023-03-06 17:55:00 +0100

    Update https protocole

--- src/components/Navbar/Search.js
+++ src/components/Navbar/Search.js
@@ -21,7 +21,7 @@ export default function Search(props) {
     <form className="form">
       <input type="search" id="query" className='form__search-term' name="q" placeholder="Search" onChange={handleInput} required />
       <button className='form__search-button' onClick={handleButtonClick}>
-        <svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+        <svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="https://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
         </svg>
       </button>
